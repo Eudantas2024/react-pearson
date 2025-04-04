39d620f93458845445c217579205efb1cb1c9edd
EUDANTAS RODRIGUES ALVES <eudantas.ralves@senacsp.edu.br>
2025-04-03 22:12:18 -0300

    react pearson

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Footer } from "./components/Footer"
+import { Header } from "./components/Header"
+import { Contato } from "./pages/Contato"
+
+
+function App() {
+  return (
+    <>
+       <Router>
+         <Header />
+           <Routes>
+              <Route path="/" element={<></>} />
+              <Route path="/contato" element={<Contato />} />
+           </Routes>
+         <Footer />
+
+       </Router>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Contato.jsx
@@ -0,0 +1,10 @@
+import styles from "./Contato.module.css";
+
+export function Contato() {
+  return (
+    <div>
+        Contato
+      
+    </div>
+  )
+}
