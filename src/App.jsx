0e94a9972e48cb87869d6258dd5dcaac3737bf3f
EUDANTAS RODRIGUES ALVES <eudantas.ralves@senacsp.edu.br>
2025-04-07 22:18:30 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Footer } from "./components/Footer"
-import { Header } from "./components/Header"
-import { Contato } from "./pages/Contato"
-
+//npm install react router dom
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Contato } from "./pages/Contato";
 
 function App() {
   return (
     <>
-       <Router>
-         <Header />
-           <Routes>
-              <Route path="/" element={<></>} />
-              <Route path="/contato" element={<Contato />} />
-           </Routes>
-         <Footer />
-
-       </Router>
-     
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+// Router por fora
+// Header e Footer fixo
+// No meio de Header e do Footer vão ter rotas
